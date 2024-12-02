37d5ef02db0420338708072077045ee5d5b2174e
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2024-12-02 08:40:50 +0530

    feat(emailList): add pagination

--- src/app/components/email-list/email-list.component.ts
+++ src/app/components/email-list/email-list.component.ts
@@ -6,40 +6,50 @@ import { EmailStateService } from '../../services/email-state.service';
 import { EmailCardComponent } from "../email-card/email-card.component";
 import { Observable } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-email-list',
   standalone: true,
-  imports: [CommonModule, EmailCardComponent, MatCardModule],
+  imports: [CommonModule, EmailCardComponent, MatCardModule, MatPaginatorModule],
   templateUrl: './email-list.component.html',
   styleUrl: './email-list.component.scss'
 })
 export class EmailListComponent {
   emails: Email[] = [];
+  paginatedEmails: Email[] = [];
   selectedEmailId$: Observable<string | null>;
+  pageSize = 5;
+  currentPage = 0;
 
   constructor(
     private router: Router,
     private emailStateService: EmailStateService,
-    private cdr: ChangeDetectorRef
   ) {
     this.selectedEmailId$ = this.emailStateService.selectedEmailId$;
   }
-  
+
   ngOnInit(): void {
     this.loadEmails();
   }
 
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   if (changes['selectedEmailId$'] && this.selectedEmailId$) {
-  //     this.cdr.detectChanges(); // Ensure change detection is triggered if selectedEmailId changes
-  //   }
-  // }
-
   loadEmails(): void {
-    this.emailStateService.filteredEmailList$.subscribe(
-      emails => (this.emails = emails)
-    );
+    this.emailStateService.filteredEmailList$.subscribe(emails => {
+      this.emails = emails;
+      this.updatePaginatedEmails();
+    });
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.updatePaginatedEmails();
+  }
+
+  updatePaginatedEmails(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedEmails = this.emails.slice(startIndex, endIndex);
   }
 
   handleEmailClick(id: string) {
